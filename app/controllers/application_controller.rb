f1229d0486a72952e794aa45a8f0aad0d1b76116
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-10-03 16:38:59 -0600

    Add Create Action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+	include DeviseTokenAuth::Concerns::SetUserByToken
+	# prepend_before_filter :configure_permitted_parameters, if: :devise_controller?
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:firstName, :lastName, :userName, :cohort])
+	end
 end
---
+++ client/src/components/PostForm.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Header, Form, } from "semantic-ui-react";
+import axios from "axios"
+
+class PostForm extends React.Component {
+  state = { title: "", body: "", }
+
+  componentDidMount() {
+    if(this.props.match.params.id){
+      axios.get(`/api/posts/${this.props.match.params.id}`)
+      .then(res => {
+        const { title, body } = res.data
+        this.setState({title, body})
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  }
+
+  handleChange = (e, {name, value}) => {
+    this.setState({[name]: value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.props.match.params.id) {
+      axios.put("/api/users/:user_id/posts", this.state)
+      .then(res => {
+        this.props.history.push("/api/users/:user_id/posts")
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    } else {
+      axios.post("/api/users/:user_id/posts", this.state)
+      .then( res => {
+        this.props.history.push("/posts")
+      })
+      .catch ( err => {
+        console.log(err)
+      })
+    }
+  }
+ 
+  render() {
+    const { title, body, } = this.state;
+    return (
+      <div>
+        <Header as="h1">{this.props.match.params.id ? "Edit Your Post" : "Make A New Post"}</Header>
+        <Form onSubmit={this.handleSubmit}>
+            <Form.Input
+              required
+              placeholder="Title"
+              label="Title"
+              name="title"
+              value={title}
+              onChange={this.handleChange}
+            />
+            <Form.TextArea
+              required
+              placeholder="Body"
+              label="Body"
+              name="body"
+              value={body}
+              onChange={this.handleChange}
+            />
+          <Form.Button color="blue">Submit</Form.Button>
+        </Form>
+      </div>
+    );
+  };
+};
+
+export default PostForm;
